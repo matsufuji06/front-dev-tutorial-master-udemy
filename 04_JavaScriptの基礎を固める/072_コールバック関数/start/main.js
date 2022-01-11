afae7e02e2b96a71ceae2af84c69c88c8e982031
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-11 19:32:58 +0900

    コールバック関数のレクチャー

--- 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
+++ 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
@@ -1,11 +1,41 @@
-function hello(callback, lastname) {
-  console.log("hello " + callback(lastname));
+// 引数に「関数」を渡す関数⇨コールバック関数
+
+// function hello(callback, lastname) {
+  //   console.log('hello ' + callback(lastname));
+  // }
+  
+// function getName() {
+//   return 'Matsufuji Naoya';
+// }
+    
+// const getFirstName = function() {
+//   return "Naoya";
+// }
+
+// hello(getName);
+// hello(function(name) {
+//   return 'Naoya ' + name;
+// }, 'Matsufuji');
+
+// ↓アロー関数で記述
+// hello(() => 'Naoya');
+
+// hello(getFirstName);
+
+// ------------------------------------------
+// 引数に「関数」を渡す関数⇨コールバック関数
+function doSomething(a, b, callback) {
+  const result = callback(a, b);
+  console.log(result);
 }
 
-function getName() {
-  return "Matsufuji";
+function multiply(a, b) {
+  return a * b;
 }
 
-hello(function(name) {
-  return "Matsufuji" + name;
-}, "Mafia");
+function plus(a, b) {
+  return a + b;
+}
+        
+doSomething(2, 4, multiply);
+doSomething(3, 8, plus);
