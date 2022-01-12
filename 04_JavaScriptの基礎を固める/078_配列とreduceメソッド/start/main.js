853fb0fade2f36a298b213d4c89b961d72d849dd
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-12 21:49:02 +0900

    reduceメソッドのレクチャー（意味わからん）

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,6 +1,16 @@
-const arry = [1,2,3,4,5];
+const arry = [1, 2, 3, 4, 5];
 
-arry.reduce(function(accu, curr){
+arry.reduce(function(accu, curr) {
   console.log(accu, curr);
-  // return accu + curr;
+  return accu + curr;
 }, 10);
+
+const str = 'animation';
+const strArry = str.split('');
+console.log(strArry);
+
+const result = strArry.reduce((accu, curr) => {
+  return accu + '<' + curr + '>';
+}, "")
+
+console.log(result);
