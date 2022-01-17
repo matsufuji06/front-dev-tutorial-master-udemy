90f9faf0679d0ca547aa91453afbf46bf781a5a3
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-17 21:08:09 +0900

    ボタンクリック時のイベントリスナを記述

--- 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -1,11 +1,4 @@
-const btn = document.querySelector("#btn");
-const h1 = document.querySelector("h1");
-
-const hello = function() {
-  // alert("hello");
-  // this.style.color = "red";
-  h1.style.color = "red";
-}
-
-btn.addEventListener("click", hello);
-h1.addEventListener("click", hello);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function() {
+  alert('hello');
+})
