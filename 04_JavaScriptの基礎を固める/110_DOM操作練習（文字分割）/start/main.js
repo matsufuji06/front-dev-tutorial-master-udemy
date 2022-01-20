6681b9fa9f16f20513429f216e061131edd70878
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-20 20:43:18 +0900

    JS上でspanタグを再現し、文字をアニメーションを復活

--- 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-  alert("hello");
   const el = document.querySelector('.animate-title');
-  console.log(el.innerHTML.trim());
+  // console.log(el.innerHTML.trim());
+  const str = el.innerHTML.trim();
+  let concatStr = '';
+  for(let c of str) {
+    // console.log(`<span class="char">${c}</span>`);
+    concatStr += `<span class="char">${c}</span>`;
+  }
+  // console.log(concatStr);
+  el.innerHTML = concatStr;
 });
