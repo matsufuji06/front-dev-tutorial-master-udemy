73128f03d0556220e2cb466a76ff64790d4eb51d
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-20 20:48:37 +0900

    .replace(' ', '&nbsp;');で半角スペースを表現

--- 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function(){
   // console.log(el.innerHTML.trim());
   const str = el.innerHTML.trim();
   let concatStr = '';
+
   for(let c of str) {
     // console.log(`<span class="char">${c}</span>`);
+    c = c.replace(' ', '&nbsp;');
     concatStr += `<span class="char">${c}</span>`;
   }
   // console.log(concatStr);
