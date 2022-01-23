1ff1020282d850a9f8d4bcb4710771ededf04d2f
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-23 14:12:50 +0900

    長方形が画面上にあるないでログのメッセージを変更

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -1,7 +1,16 @@
 const child = document.querySelector('.child');
-const cb = function() {
-  alert('intersecting');
-  
+const cb = function(entries, observer) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      // ※画面上に要素が入っているとき
+      console.log('inview');
+    } else {
+      // ※要素が画面上から出たとき
+      console.log('out view');
+    }
+  });
+  // alert('intersecting');
+
 }
 const io = new IntersectionObserver(cb);
 io.observe(child);
