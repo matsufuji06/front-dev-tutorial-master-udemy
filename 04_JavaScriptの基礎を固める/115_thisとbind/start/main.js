e413bf363494511817c11697c1c25a6eb5e8c188
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-23 10:28:42 +0900

    bindでthisの参照先を指定

--- 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
+++ 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
@@ -3,10 +3,18 @@ const obj = {
     last_name: 'Code',
     printFullName: function () {
         console.log(this);
-
-        window.setTimeout(function () {
+        const _that = this;
+        
+        window.setTimeout(function() {
             console.log(this);
-        });
+            // ↓のbindで実質、thisには_thatが定義されている（thisの参照先がbindの引数になる⇨引数を{first_name: 'Taro'のように特定のキー情報にしてもよい}）
+        }.bind(_that));
+
+        // window.setTimeout(function () {
+        //     console.log(this);
+        //     ※↓外で変数として「objのthis」を呼び出せば「windowのthis」を参照せずに済む
+        //     console.log(_that);
+        // });
     }
 }
 
