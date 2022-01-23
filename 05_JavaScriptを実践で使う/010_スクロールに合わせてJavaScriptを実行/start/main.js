b5d329a54b9db6b07864eaf3471919af69b8cf04
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-23 14:19:15 +0900

    長方形が画面から出た・出ないでクラスを付与・削除し、スタイルを適用

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -4,9 +4,13 @@ const cb = function(entries, observer) {
     if(entry.isIntersecting) {
       // ※画面上に要素が入っているとき
       console.log('inview');
+      // ※↓以降、監視をしないようにする
+      // observer.unobserve(entry.target);
+      entry.target.classList.add('inview');
     } else {
       // ※要素が画面上から出たとき
       console.log('out view');
+      entry.target.classList.remove('inview');
     }
   });
   // alert('intersecting');
