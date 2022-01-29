a17d61c766fe6e8a289120ef44183b514c964209
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-29 11:50:53 +0900

    プライベート関数内にクリックイベントを切り分け

--- 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -1,13 +1,18 @@
 class MobileMenu {
   constructor() {
     this.DOM = {};
-    this.DOM.btn = document.querySelector('.mobile-menu__btn')
-    this.DOM.container = document.querySelector('#global-container')
-    this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+    this.DOM.btn = document.querySelector('.mobile-menu__btn');
+    this.DOM.container = document.querySelector('#global-container');
+    this._addEvent();
   }
-
+  
   _toggle() {
     this.DOM.container.classList.toggle('menu-open');
+  }
+  
+  _addEvent() {
+    this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+    
   }
 }
 
