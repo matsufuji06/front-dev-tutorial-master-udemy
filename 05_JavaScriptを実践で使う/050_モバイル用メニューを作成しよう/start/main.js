321b2d326bbf80f887badb248350d786e7e57651
matsufuji06 <n.matsufuji06@gmail.com>
2022-01-29 11:58:51 +0900

    スマホの"ontouchstart"イベントと'click'イベントの条件分岐

--- 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -3,15 +3,27 @@ class MobileMenu {
     this.DOM = {};
     this.DOM.btn = document.querySelector('.mobile-menu__btn');
     this.DOM.container = document.querySelector('#global-container');
+    this.eventType = this._getEventType();
     this._addEvent();
   }
   
+  _getEventType() {
+    // ↓スマホで"ontouchstart"イベントが検知できるかどうかの記述。複雑なのであんまり気にしない。
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof window.DocumentTouch) ||
+      navigator.maxTouchPoints > 0 ||
+      window.navigator.msMaxTouchPoints > 0;
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
+
   _toggle() {
     this.DOM.container.classList.toggle('menu-open');
   }
   
   _addEvent() {
-    this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
     
   }
 }
