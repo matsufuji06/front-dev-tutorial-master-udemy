6d035c807511cff7169a09b205d8ea56e60bd43f
matsufuji06 <n.matsufuji06@gmail.com>
2022-02-06 11:31:04 +0900

    Swiperのプロパティと値を設定

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -6,9 +6,23 @@ class HeroSlider {
   constructor() {
     this.swiper = new Swiper('.swiper', {
       // Optional parameters
-      direction: 'vertical',
+      // direction: 'vertical',
       loop: true,
-    
+      grabCursor: true,
+      effect: 'coverflow',
+      centeredSlides: true,
+      slidesPerView: 1,
+      speed: 1000,
+      breakpoints: {
+        1024: {
+          slidesPerView: 2
+        }
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      }
+
       // If we need pagination
       // pagination: {
       //   el: '.swiper-pagination',
