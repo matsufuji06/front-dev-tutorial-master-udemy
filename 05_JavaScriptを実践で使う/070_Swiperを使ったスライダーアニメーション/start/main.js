312ca36c517aadaa8650ad2248bc42c96c0e9eee
matsufuji06 <n.matsufuji06@gmail.com>
2022-02-06 11:13:49 +0900

    DOMContentLoaded時にSwiperのクラスを生成

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,21 +1,29 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider();
 });
+
+class HeroSlider {
+  constructor() {
+    this.swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+    
+      // If we need pagination
+      // pagination: {
+      //   el: '.swiper-pagination',
+      // },
+    
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+    
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+  }
+};
