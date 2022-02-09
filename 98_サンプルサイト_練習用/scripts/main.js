db34314a50d0add828400f3d63d82cc41769723d
matsufuji06 <n.matsufuji06@gmail.com>
2022-02-09 16:31:42 +0900

    style&JSの読み込み

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -4,5 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(() => {
     hero.stop();
   }, 5000);
+
+  const cb = function(el, isIntersecting) {
+    if(isIntersecting) {
+        const ta = new TextAnimation(el);
+        ta.animate();
+    }
+  }
+
+  const so = new ScrollObserver('.animate-title', cb);
 });
 
