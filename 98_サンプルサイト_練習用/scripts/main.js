34a000b47ecde926c71d3d07d87682d1da81518d
matsufuji06 <n.matsufuji06@gmail.com>
2022-02-10 19:45:03 +0900

    houseの画像を設置

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -6,12 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
   }, 5000);
 
   const cb = function(el, isIntersecting) {
-    if(isIntersecting) {
+    if(inview) {
         const ta = new TweenTextAnimation(el);
         ta.animate();
     }
   }
 
   const so = new ScrollObserver('.tween-animate-title', cb);
+
+  const __inviewAnimation = function(el, inview) {
+    if(inview) {
+      el.classList.add('inview');
+    } else {
+      el.classList.remove('inview');
+    }
+  }
+
+  const so2 = new ScrollObserver('.cover-slide', __inviewAnimation);
 });
 
